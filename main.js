45904dac606ad3b5423a4f3729ebb19550cff12f
AymanGhaith <ayman.ghaith88@gmail.com>
2018-03-01 14:35:58 +0200

    adding new version with new features such as :1-users list 2- display user info 3- creat new user

--- main.js
+++ main.js
@@ -2,7 +2,31 @@
 var resultFlag=false;
 // resultArray is array to save the results to do some functionalities on it .
 var  resultArray = [];
+
+//users
+var user1={name:"user1",numOfTry:1,numOfSuccess:1}
+var user2={name:"user2",numOfTry:0,numOfSuccess:0}
+var user3={name:"user3",numOfTry:0,numOfSuccess:0}
+var user4={name:"user4",numOfTry:0,numOfSuccess:0}
+var user5={name:"user5",numOfTry:0,numOfSuccess:0}
+
+//users array
+var users=[user1,user2,user3,user4,user5]
+
+//factory function
+function fact(name){
+	return {
+		name:name,
+		numOfTry:0,
+		numOfSuccess:0
+	}
+}
+
+
 $(document).ready(function(){
+	//default hide for game divs:
+	$(".selectUser").hide();
+	$(".top10").hide();
 
 	//This is the event of numbers
 	//
@@ -21,6 +45,27 @@ $(document).ready(function(){
 			if(resultFlag===true || result===undefined){
 				return 0;
 			}
+			if($("#gameMode").is(":checked")){
+				var i=$("select").val()
+				if((!(i>=0))||i===null){
+					alert("Please select user");
+					return 0
+				}
+
+				if($("#guessText").val()===""){
+					alert("please guess the number");
+					$("#guessText").css("background-color","red");
+					return 0;
+				}
+				var user=users[i]
+				user.numOfTry+=1;
+
+				if($("#guessText").val()==result){
+					user.numOfSuccess+=1;
+				}
+				$("#userInfo").html("Name: "+user.name + "<br>Number of tries: "+user.numOfTry+"<br>Number of Success: "+user.numOfSuccess+ "<br>Score: "+Math.round((user.numOfSuccess/user.numOfTry*100))+"%");
+
+			}
 			$("#text").val(result);
 			resultFlag=true;
 			resultArray.push(result);
@@ -37,6 +82,8 @@ $(document).ready(function(){
 	$("#cls").on("click",function(){
 		$("#text").val("");
 		$("#text").css("background-color","");
+		$("#guessText").css("background-color","");
+		$("#guessText").val("");
 	});
 
 	//This is the clear the latest change on the text box
@@ -126,9 +173,39 @@ $(document).ready(function(){
 		var guess=$('<input type="text" name="text" id="guessText" placeholder="Guess the number">')
 		if (this.checked) {
 			$(this).after(guess);
+			$(".selectUser").show();
+			$(".top10").show();
 		} else {
 			$("#guessText").remove();
+			$(".selectUser").hide();
+			$(".top10").hide();
 		}
+	});
+
+	// fill users select:
+		for (var i = 0; i < users.length; i++) {
+			var user=users[i].name;
+			var option=$('<option value='+i+'>'+user+'</option>');
+			$("select").append(option);
+		}
+
+	$("select").on("change",function(){
+		var user=users[$("select").val()];
+		$("#userInfo").html("Name: "+user.name + "<br>Number of tries: "+user.numOfTry+"<br>Number of Success: "+user.numOfSuccess+ "<br>Score: "+Math.round((user.numOfSuccess/user.numOfTry*100))+"%");
+	});
+
+	
+	$("#addUser").on("click",function(){
+		if($("#newUser").val()===""){
+			alert("Please add the user name");
+			return 0;
+		}
+		var i = users.push(fact($("#newUser").val()));
+		var option=$('<option value='+(i-1)+'>'+$("#newUser").val()+'</option>');
+		$("select").append(option);
+		$("#newUser").val("");
+		alert("User Added successfully");
+
 	});
 
 })
